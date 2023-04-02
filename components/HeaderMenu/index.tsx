962f69fef9ed434bddfe4e4eb0aaf2770ab91257
Luis Abellan <luis.abellan.dev@gmail.com>
2023-04-02 22:34:11 +0200

    position header menu items to the right and added some padding to items

--- components/HeaderMenu/index.tsx
+++ components/HeaderMenu/index.tsx
@@ -1,25 +1,36 @@
 import Link from 'next/link'
-import styles from '@/styles/Home.module.scss'
+import Image from 'next/image'
+import styles from '@/styles/HeaderMenu.module.scss'
 
 export default function HeaderMenu() {
   return (
-    <>
-      <div className={styles.menuHeader}>
-        <div className="flex flex-row">
-          <li className="list-none">
+    <div className="flex flex-row">
+      <div className="flex-end w-9/12">
+        <Image
+          src="/logos/logo.png"
+          alt="LoLFans Logo"
+          className=""
+          width={50}
+          height={50}
+          priority
+        />
+      </div>
+      <div className="flex flex-row w-full justify-end items-center">
+        <div className='flex'>
+          <li className="list-none pr-6">
             <Link href="/">Home</Link>
           </li>
-          <li className="list-none">
+          <li className="list-none pr-6">
             <Link href="/players">Players</Link>
           </li>
-          <li className="list-none">
+          <li className="list-none pr-6">
             <Link href="/champions">Champions</Link>
           </li>
-          <li className="list-none">
+          <li className="list-none  pr-6">
             <Link href="/events">Events</Link>
           </li>
         </div>
       </div>
-    </>
+    </div>
   )
 }
